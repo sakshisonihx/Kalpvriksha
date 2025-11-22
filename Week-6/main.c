9148b0315db2f3cc891d9eb3342adb74f2237436
sakshi <sakshi53929soni@gmail.com>
2025-11-22 11:26:45 +0530

    implemented createCache function

--- Week-6/main.c
+++ Week-6/main.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "data_structures.h"
 
 int main()
 {
-
+    int size;
+    printf("Enter size: ");
+    if (scanf("%d", size) != 1)
+    {
+        printf("\nError. Enter number.");
+        clearBuffer();
+        return;
+    };
+    if (size < 1 || size > 1000)
+    {
+        printf("\nSize should be between 1 and 1000");
+        return;
+    }
     return 0;
+    createCache(size);
+}
+
+// clearing buffer
+void clearBuffer()
+{
+    int character;
+    while ((character = getchar()) != '\n' && character != EOF)
+        ;
 }
 
 void createCache(int size)
 {
+    LRUCapacity = size;
+    hashTable = (HashNode *)calloc(size, sizeof(HashNode));
+    if (hashTable == NULL)
+    {
+        printf("\nMemory allocation failed.");
+        return;
+    }
 }
 
 char *get(int key)
--- Week-6/structure_implementation.c
+++ Week-6/structure_implementation.c
@@ -1,6 +1,10 @@
 #include "data_structures.h"
 #include <stdio.h>
 
+HashNode *hashTable = NULL;
+Queue LRUCache = {.front = NULL, .rear = NULL};
+int LRUCapacity = 0;
+
 int getHashIndex(int key)
 {
     return key % LRUCapacity;
