1b8210da176394f2c275ea2d47acaba990ebfed9
sakshi <sakshi53929soni@gmail.com>
2025-12-06 18:49:34 +0530

    function to take user input

--- Week-7/main.c
+++ Week-7/main.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 #include "data_structures.h"
+
+#define INPUT_SIZE 100
+
+void readLine(char *);
 
 int main()
 {
-
+    char inputBuffer[INPUT_SIZE];
+    while (1)
+    {
+        if (!fgets(inputBuffer, INPUT_SIZE, stdin))
+            break;
+        inputBuffer[strcspn(inputBuffer, "\n")] = '\0';
+        if (strcmp(inputBuffer, "END") == 0)
+        {
+            break;
+        }
+        readLine(inputBuffer);
+    }
     return 0;
+}
+
+void readLine(char *line)
+{
+    if (strlen(line) == 0)
+    {
+        return;
+    }
+
+    if (strncmp(line, "KILL", 4) == 0)
+    {
+        int pid, killTime;
+        if (sscanf(line, "KILL %d %d", &pid, &killTime) == 2)
+        {
+            printf("\nKILL command, pid = %d, kill time = %d", pid, killTime);
+        }
+        else
+        {
+            printf("\nInvalid command");
+        }
+    }
+    else
+    {
+        char name[50];
+        int pid, burstTime, ioTime, ioDuration;
+        if (sscanf(line, "%s %d %d %d %d", name, &pid, &burstTime, &ioTime, &ioDuration) == 5)
+        {
+            printf("\nProcess detail, pid = %d, name = %s, burstTime = %d, iotime = %d, ioDuration = %d", pid, name, burstTime, ioTime, ioDuration);
+        }
+        else
+        {
+            printf("\nInvalid command");
+        }
+    }
 }
